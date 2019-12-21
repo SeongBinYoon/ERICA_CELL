1461bd403c66c7101a2aa7e17368b28e56f9c6f7
SeongBinYoon <yoonseongbin4925@gmail.com>
2019-12-21 15:13:50 +0900

    input number

--- src/game.py
+++ src/game.py
@@ -18,3 +18,57 @@ def number_baseball():
             result = True
         return result 
 
+    def is_duplicated_number(three_digit):
+        temp1 = three_deigit // 100
+        temp2 = (three_digit - temp1 * 100) // 10
+        temp3 = three_digit - temp1 * 100 - temp2 * 10
+
+        if temp1 == temp2:
+            result = True
+        elif temp1 == temp3:
+            result = True
+        elif temp2 == temp3:
+            result = True
+        else:
+            result = False
+        return result
+
+    def is_validated_number(user_input_number):
+        valid_num = is_digit(user_input_number)
+        temp = 0
+        if valid_num == True:
+            temp = int(user_input_number)
+            if is_between_100_and_999(temp) == True:
+                result = temp
+                if is_duplicated_number(temp) == False:
+                    temp = temp
+                else:
+                    print("0부터 9까지의 숫자를 한번씩만 사용해주세요.")
+                    result = None
+
+            else:
+                print("조건에 맞게 숫자를 입력해주세요.")
+                result = None
+        else:
+            print("은(는) 숫자가 아닙니다. 다시 입력해주세요.")
+            result = None
+        return result
+
+    def get_not_duplicated_three_digit_number():
+        result = None
+        while 1:
+            rnum = get_random_number()
+            temp1 = rnum // 100
+            temp2 = (rnum - temp1 * 100) // 10
+            temp3 = rnum - temp1 * 100 - temp2 * 10
+            if temp1 == temp2:
+                result = None
+            elif temp1 == temp3:
+                result = None
+            elif temp2 == temp3:
+                result = None
+            else:
+                result = rnum
+                break
+        return result
+
