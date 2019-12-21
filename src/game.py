259cb21d7db445ca73c05a265a3b6922498b22c8
HyunJin <hikhjin@gmail.com>
2019-12-21 15:00:12 +0900

    numberbaseball

--- src/game.py
+++ src/game.py
@@ -1,2 +1,20 @@
-print("(치지지지직)TV: 
+import random
+
+#숫자야구게임
+def number_baseball():
+    def get_random_number():
+        return random.randrange(100, 1000)
+    def is_digit(user_input_number):
+        if user_input_number.isdigit() == True: 
+            result = True
+        else :
+            result = False
+        return result
+
+    def is_between_100_and_999(user_input_number): 
+        if (((user_input_number//100)<1) | ((user_input_number//1000)>=1)):
+            result = False
+        else:
+            result = True
+        return result 
 
